cf1ceaaf7e03abd1f62df9d64ac2a5277ae156f8
rreddyga <ramanag2299@gmail.com>
2026-02-02 22:05:00 +0530

    roboshop script file added

---
+++ roboshop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+SG_ID="sg-0d4fbbcb5f7b89575"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@ # user may pass mongo,cataglouge
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+    if [ $instance == "frontend"]; then
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[0].Instances[0].PublicIpAddress' \
+        --output text)
+    else
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+        --output text)
+    fi
+    echo  "IP Address :$IP"
+done
